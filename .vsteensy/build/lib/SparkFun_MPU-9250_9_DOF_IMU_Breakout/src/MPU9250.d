26219b7a686ed4acad69a61799780e6081d62b3f
jsuper2 <jsuper2@illinois.edu>
2019-11-21 00:18:02 -0600

    Added some libraries

---
+++ .vsteensy/build/lib/SparkFun_MPU-9250_9_DOF_IMU_Breakout/src/MPU9250.d
@@ -0,0 +1,32 @@
+.vsteensy/build/lib/SparkFun_MPU-9250_9_DOF_IMU_Breakout/src//MPU9250.o: \
+ lib/SparkFun_MPU-9250_9_DOF_IMU_Breakout/src//MPU9250.cpp \
+ lib/SparkFun_MPU-9250_9_DOF_IMU_Breakout/src//MPU9250.h lib/SPI/SPI.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WProgram.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/pgmspace.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/interrupt.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr_functions.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/wiring.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/binary.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/core_id.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/core_pins.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/imxrt.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/pins_arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/HardwareSerial.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Stream.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Print.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WString.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr/pgmspace.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Printable.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/avr_emulation.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_serial.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_desc.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_seremu.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/usb_rawhid.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/WCharacter.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/elapsedMillis.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/Arduino.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/IntervalTimer.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/DMAChannel.h \
+ C:\PROGRA~2\Arduino\hardware\teensy\avr\cores\teensy4/EventResponder.h \
+ lib/Wire/Wire.h lib/Wire/WireIMXRT.h
